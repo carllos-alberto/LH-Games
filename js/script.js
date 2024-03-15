29a72688b432a0536688ef8b7235281af50abfba
carllos alberto <carllos.seg@gmail.com>
2024-03-14 21:01:22 -0300

    Conclusão do projeto

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
 //efeito do botão voltar ao Topo
 
+function topo() {
+    window.scrollTo(
+        {
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        }
+    )
+}
+
+
+
 //Validação de Login
 
+function login() {
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if (usuario == 'admin' && senha == '123456') {
+        window.location = "index.html";
+        logado = 1;
+    }
+
+    if (logado == 0) {
+        alert('acesso Negado. Dados incorretos.')
+    }
+}
+
+
+
+
+
 //Ativar alert no botão cadastrar
+
+function cadastro() {
+    alert("Cadastrado com sucesso!");
+    window.location.href = "index.html";
+}
